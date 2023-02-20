c7823f16e2d872074a4262c332c931421948f512
Raam Noble <raam13@outlook.com>
2023-02-19 20:03:32 -0800

    Added GET endpoint for /weather-by-zipcode

--- src/routes/currentWeather.js
+++ src/routes/currentWeather.js
@@ -3,7 +3,9 @@ const validator = require("validator");
 const path = require("path");
 
 const getCurrentWeather = require("../services/getCurrentWeather");
+const getCoordinatesByZipcode = require("../services/getCoordinatesByZipcode");
 const logger = require("../utils/logger");
+const { count } = require("console");
 
 const filename = path.basename(__filename);
 const router = new express.Router();
@@ -13,14 +15,13 @@ router.get("/weather-by-coordinates", async (req, res) => {
   try {
     const { lat, lon } = req.body;
 
+    // Validation logic to ensure lat and lon values are set properly within the request body
     if (!lat || !lon) {
       throw new Error("Invalid body");
     }
-
     if (typeof lat === "string" && !validator.isNumeric(lat)) {
       throw new Error("Invalid lattitude");
     }
-
     if (typeof lon === "string" && !validator.isNumeric(lon)) {
       throw new Error("Invalid longitude");
     }
@@ -28,15 +29,61 @@ router.get("/weather-by-coordinates", async (req, res) => {
     try {
       const result = await getCurrentWeather(lat, lon);
 
-      logger.info(`${filename}: ${JSON.stringify(result)}`);
+      logger.info(
+        `${filename}: /weather-by-coordinates: ${JSON.stringify(result)}`
+      );
       res.status(200).send(result);
     } catch (e) {
-      logger.info(`${filename}: ${e.stack}`);
+      logger.error(`${filename}: /weather-by-coordinates: ${e.stack}`);
       res.status(500).send();
     }
   } catch (e) {
-    logger.info(`${filename}: ${e.stack}`);
+    logger.error(`${filename}: /weather-by-coordinates: ${e.stack}`);
     res.status(400).send(e.message);
+  }
+});
+
+// Get weather by coordinates
+router.get("/weather-by-zipcode", async (req, res) => {
+  const { zipcode, country } = req.query;
+
+  // Validation logic to ensure zipcode is valid - if country is blank assume it belongs to US
+  if (!zipcode) {
+    res.status(400).send("Zipcode is required");
+    return;
+  }
+  if (!validator.isPostalCode(zipcode, country ? country : "US")) {
+    res.status(400).send("Invalid zipcode or country");
+    return;
+  }
+
+  // Retrieve coordinates using the input zipcode
+  let lat, lon;
+  try {
+    const coordinatesResult = await getCoordinatesByZipcode(zipcode, country);
+    lat = coordinatesResult.lat;
+    lon = coordinatesResult.lon;
+    logger.info(
+      `${filename}: /weather-by-zipcode: Lattitude & Longitude retrieved: ${lat}, ${lon}`
+    );
+  } catch (e) {
+    logger.error(`${filename}: /weather-by-zipcode: ${e.stack}`);
+    res.status(400).send("Invalid zipcode or country");
+    return;
+  }
+
+  try {
+    // Fetch the current weather after geocoding zipcode into coordinates
+    const weatherResult = await getCurrentWeather(lat, lon);
+    logger.info(
+      `${filename}: /weather-by-zipcode: ${JSON.stringify(weatherResult)}`
+    );
+    res.status(200).send(weatherResult);
+    return;
+  } catch (e) {
+    logger.error(`${filename}: /weather-by-zipcode: ${e.stack}`);
+    res.status(500).send();
+    return;
   }
 });
 
